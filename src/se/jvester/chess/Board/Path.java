ebe29d4d14621f5d9c5d7e6a9cdc1dad93194226
Johan Vester <jv@jv-macbook.local>
2016-09-22 17:12:25 +0200

    Remove unused field

--- src/se/jvester/chess/Board/Path.java
+++ src/se/jvester/chess/Board/Path.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Path {
-    private Square destination;
     private List<Position> nodes = new ArrayList<>();
 
     public Path() {}
